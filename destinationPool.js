69621ba71492c13e8da34875fbb95853bd0a96ed
Andrei Sterin <andrei.sterin92@gmail.com>
2019-11-05 00:40:52 +0100

    Renamed Memory.creep_destination to Memory.m_creep_destination

--- destinationPool.js
+++ destinationPool.js
@@ -21,7 +21,7 @@ var destinationPool =  {
     cleanup_dead_creeps: function(type) {
         var alive_list = this.alive_list(type);
         var names_to_delete = [];
-        for (var name in Memory.creep_destination) {
+        for (var name in Memory.m_creep_destination) {
             var id = Game.creeps[name].id;
             if (!alive_list.findIndex(id)) {
                 names_to_delete.append(name);
@@ -38,8 +38,8 @@ var destinationPool =  {
 
         // Build destination object now.
         var sources_map = this.empty_sources_map(destination_list);
-        for (id in Memory.creep_destination) {
-            var destination = Memory.creep_destination[id];
+        for (id in Memory.m_creep_destination) {
+            var destination = Memory.m_creep_destination[id];
             sources_map[destination] = sources_map[destination] + 1;
         }
         return sources_map;
@@ -48,11 +48,11 @@ var destinationPool =  {
         var destination_list = this.destination_list(type);
         Memory.destination = this.create_sources_map_from_scratch();
         Memory.temp = destination_list;
-        Memory.creep_destination = {};
+        Memory.m_creep_destination = {};
     },
     get: function(type, name) {
-        if (Memory.creep_destination[name]) {
-            var id = Memory.creep_destination[name];
+        if (Memory.m_creep_destination[name]) {
+            var id = Memory.m_creep_destination[name];
             return Game.getObjectById(id);
         }
         var destinations = this.destination_list(type);
@@ -61,15 +61,15 @@ var destinationPool =  {
             if (Memory.destination[id] < 1) {
                 console.log(Memory.destination[id]);
                 Memory.destination[id] = Memory.destination[id] + 1;
-                Memory.creep_destination[name] = id;
+                Memory.m_creep_destination[name] = id;
                 return Game.getObjectById(id);
             }
         }
     },
     free: function(type, name) {
-        if (Memory.creep_destination[name]) {
-            var id = Memory.creep_destination[name];
-            delete Memory.creep_destination[name];
+        if (Memory.m_creep_destination[name]) {
+            var id = Memory.m_creep_destination[name];
+            delete Memory.m_creep_destination[name];
             Memory.destination[id] = Memory.destination[id] - 1;
         }
     }
